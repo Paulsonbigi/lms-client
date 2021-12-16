c27c195fca9a68a0034b95ca4bd31d89a4bdd93d
Adegbokan <adegbokanpaul@gmail.com>
2021-12-16 23:03:12 +0100

    new changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -68,7 +68,7 @@ export default {
   // },
 
    axios: {
-    baseURL: "https://lms-servers.herokuapp.com"  // here set your API url
+    baseURL: "https://lms-servers.herokuapp.com/"  // here set your API url
   },
 
 
